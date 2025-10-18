b514e2a81897ec15808dfb183a37a71c07f54e84
ew-prog <ewanyama@live.co.uk>
2025-10-18 11:49:48 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import PartnerPortal from './pages/PartnerPortal'
 import LeadsDashboard from './pages/LeadsDashboard'
 import Login from './pages/Login'
-import { Navigate } from 'react-router-dom'
 
 function AuthRoute({ children }) {
-  const token = localStorage.getItem('token')
-  return token ? children : <Navigate to="/login" />
+  return localStorage.getItem('token') ? children : <Navigate to="/login" />
 }
 
 function App() {
