0aa3bacfca78bbf7d0a89fec0f617c4f48f2d6c8
ew-prog <ewanyama@live.co.uk>
2025-10-18 02:22:50 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './Navbar'
+import Navbar from './Navbar"
 import Home from './Home'
 import PartnerPortal from './PartnerPortal'
 import LeadsDashboard from './LeadsDashboard'
