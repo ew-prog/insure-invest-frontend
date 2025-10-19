35cbb0344d3a99d555e255a74680eb7d7d52126e
ew-prog <ewanyama@live.co.uk>
2025-10-19 12:42:47 -0700

    Create Footer.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-brand-dark-green text-white p-4 mt-10 text-center">
+      <p className="text-sm">
+        © 2025 InsureInvest. All rights reserved.
+      </p>
+    </footer>
+  );
+}
