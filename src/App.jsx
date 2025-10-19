b9d95906c99eb721f0954cf8db31eb1d5695d175
ew-prog <ewanyama@live.co.uk>
2025-10-19 13:53:11 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LeadForm from "./LeadForm";
+import ProductPage from "./ProductPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LeadForm />} />
+        <Route path="/product/:productName" element={<ProductWrapper />} />
       </Routes>
     </Router>
   );
+}
+
+// Wrapper to get productName from URL
+import { useParams } from "react-router-dom";
+function ProductWrapper() {
+  const { productName } = useParams();
+  return <ProductPage productName={decodeURIComponent(productName)} />;
 }
