e9b12cfdb2e390f9f082fa87406afc7ac42193a1
ew-prog <ewanyama@live.co.uk>
2025-10-19 14:39:30 -0700

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,10 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="bg-brand-green text-white px-6 py-4 flex justify-between items-center flex-wrap">
-      <h1 className="font-bold text-lg">InsureInvest</h1>
+      {/* Smaller logo */}
+      <h1 className="font-bold text-lg" style={{ color: "#007847" }}>
+        InsureInvest
+      </h1>
       <ul className="flex space-x-4 mt-2 md:mt-0 flex-wrap">
         <li>
           <Link to="/product/unit-trust" className="hover:text-brand-light-green font-medium">
