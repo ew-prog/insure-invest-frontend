7053e32ff907f88626d5cc17da1bfcb96d55c305
ew-prog <ewanyama@live.co.uk>
2025-10-19 14:27:05 -0700

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="bg-brand-green text-white px-6 py-4 flex justify-between items-center flex-wrap">
       <h1 className="font-bold text-lg">InsureInvest</h1>
       <ul className="flex space-x-4 mt-2 md:mt-0 flex-wrap">
-        <li><a href="/product/unit-trust" className="hover:text-brand-light-green font-medium">Unit Trust</a></li>
-        <li><a href="/product/motor" className="hover:text-brand-light-green font-medium">Motor</a></li>
-        <li><a href="/product/home" className="hover:text-brand-light-green font-medium">Home</a></li>
-        <li><a href="/product/life" className="hover:text-brand-light-green font-medium">Life Insurance</a></li>
-        <li><a href="/product/travel" className="hover:text-brand-light-green font-medium">Travel</a></li>
-        <li><a href="/product/medical" className="hover:text-brand-light-green font-medium">Medical</a></li>
+        <li>
+          <Link to="/product/unit-trust" className="hover:text-brand-light-green font-medium">
+            Unit Trust
+          </Link>
+        </li>
+        <li>
+          <Link to="/product/motor" className="hover:text-brand-light-green font-medium">
+            Motor
+          </Link>
+        </li>
+        <li>
+          <Link to="/product/home" className="hover:text-brand-light-green font-medium">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/product/life" className="hover:text-brand-light-green font-medium">
+            Life Insurance
+          </Link>
+        </li>
+        <li>
+          <Link to="/product/travel" className="hover:text-brand-light-green font-medium">
+            Travel
+          </Link>
+        </li>
+        <li>
+          <Link to="/product/medical" className="hover:text-brand-light-green font-medium">
+            Medical
+          </Link>
+        </li>
       </ul>
     </nav>
   );
